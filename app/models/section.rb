7d9d286a7bdf2caa1cd1b623b5fdd43a5f8d87e9
Marlon Lopez <marlonlop.cs@gmail.com>
2018-10-04 23:14:51 -0600

    added selec boxes to section and enrollment forms

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,8 @@ class Section < ApplicationRecord
   belongs_to :course
   has_many :enrollments
   has_many :students, through: :enrollments
+
+  def d_section
+    "#{course.name} | Section: #{section_number} | #{semester}"
+  end
 end
