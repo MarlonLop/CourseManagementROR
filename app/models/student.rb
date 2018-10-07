3736c63c807cb4f2934b844982daf59ba85921a6
Marlon Lopez <marlonlop.cs@gmail.com>
2018-10-07 01:26:14 -0600

    added unit test to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
   has_many :enrollments
   has_many :sections, through: :enrollments
 
-  validates :name, length: 3..80, format: { with: /\A[a-zA-Z]+\z/, message:
+  validates :name, length: 3..80, format: { with: /\A[a-zA-Z\s]+\z/, message:
       'field allows only letters' }
   validates :student_number, uniqueness: true, numericality:
       { only_integer: true }, length: { is: 6 }
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -4,4 +4,33 @@ class StudentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "student attributes must not be empty" do
+    student = Student.new
+    assert student.invalid?
+    assert student.errors[:name].any?
+    assert student.errors[:student_number].any?
+  end
+
+  test "student name is only letters and within 3 to 80 chars" do
+    student = Student.new(name: "Person234 l4",
+                          student_number: 100001)
+    assert student.invalid?
+    student.name = 'me'
+    assert student.invalid?
+    student.name = 'meshfjahsljhaljhgaljhdgjahdghajgldjghlajshlgjhalsjhg
+                    dsjkhajdhs;gjhs;dhjgsjhfdjghsjsfskfdhgkhsfdkhgsfhg
+                    kdshghdkhgkhghghgkfhgjlshfljkghlsj'
+    assert student.invalid?
+    student.name = 'John Smith'
+    assert student.valid?
+  end
+
+  test "student number is 6 digits" do
+    student = Student.new(name: "Billy Bob",
+                          student_number: 10000)
+    assert student.invalid?
+    student.student_number = 123456
+    assert student.valid?
+  end
 end
