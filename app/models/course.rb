0c77a6c714b7bb8a803b062b152bb676960a8306
Marlon Lopez <marlonlop.cs@gmail.com>
2018-10-06 20:55:54 -0600

    course validations added

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,10 @@
 class Course < ApplicationRecord
   has_many :sections
+
+  # Makes sure a new course is not created with empty fields
+  #validates :name, :department, :number, :credit_hours, presence: true
+  validates :name, uniqueness: true, length: 3..80
+  validates :department, presence: true, length: { maximum: 25 }
+  validates :number, uniqueness: true, length: 5..7
+  validates :credit_hours, numericality: { only_integer: true}, length: { is: 1 }
 end
