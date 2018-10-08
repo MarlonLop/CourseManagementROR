1ef8bd80e3450401b2d93ce19548dca304ca6ef1
Marlon Lopez <marlonlop.cs@gmail.com>
2018-10-08 10:11:12 -0600

    some css changes and other minor sintax

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :courses
   resources :students
 
-  get 'courses/index'
   root 'courses#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
