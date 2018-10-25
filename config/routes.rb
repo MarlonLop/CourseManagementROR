9ae78e6285aa8a20f34ddc2ad62bf765c0dce420
Marlon Lopez <mlopez98@msudenver.edu>
2018-10-25 10:36:21 -0600

    log in - out, sign up working;added bootstrapt and new navbar

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :courses
   resources :students
 
-  root 'courses#index'
+  root to: 'courses#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
